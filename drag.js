149e0db1b13eecd29cb7b388b99d32915b0953e4
KaranamSireesha <karanamsireesha9@gmail.com>
2023-05-01 13:01:42 +0530

    new2

--- drag.js
+++ drag.js
@@ -11,34 +11,29 @@ draggables.forEach((draggable) => {
   });
 });
 
-containers.forEach((container) => {
-  container.addEventListener("dragover", (e) => {
-    e.preventDefault();
-    const afterElement = getDragAfterElement(container, e.clientY);
-    const draggable = document.querySelector(".dragging");
-    if (afterElement == null){
-        container.appendChild(draggable);
+containers.forEach(container => {
+  container.addEventListener('dragover', e => {
+    e.preventDefault()
+    const afterElement = getDragAfterElement(container, e.clientY)
+    const draggable = document.querySelector('.dragging')
+    if (afterElement == null) {
+      container.appendChild(draggable)
+    } else {
+      container.insertBefore(draggable, afterElement)
     }
-    else{
-        container.insertBefore(draggable, afterElement)
-    }
-  });
-});
+  })
+})
 
 function getDragAfterElement(container, y) {
-  const draggableElements = [
-    ...container.querySelectorAll(".draggable:not(.dragging)"),
-  ];
+  const draggableElements = [...container.querySelectorAll('.draggable:not(.dragging)')]
 
-  draggableElements.reduce(
-    (closest, child) => {
-      const box = child.getBoundingClientRect();
-      const offset = y - box.top - box.height / 2;
-      if (offset < 0 && offset > closest) {
-        return { offset: offset, element: child };
-      } else {
-        closest;
-      }
-    },{offset : Number.NEGATIVE_INFINITY}
-  )
+  return draggableElements.reduce((closest, child) => {
+    const box = child.getBoundingClientRect()
+    const offset = y - box.top - box.height / 2
+    if (offset < 0 && offset > closest.offset) {
+      return { offset: offset, element: child }
+    } else {
+      return closest
+    }
+  }, { offset: Number.NEGATIVE_INFINITY }).element
 }
